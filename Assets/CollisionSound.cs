1ef033d30ede0643bcd357d5ec00bfd9684e7fba
TomTheFurry <tomlee92502@yahoo.com>
2022-04-20 00:02:52 +0800

    Add (basic) collision sound.

---
+++ Assets/CollisionSound.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    public AudioSource collisionEffect;
+    public float minVolume;
+    public float maxVolume;
+    public float minVolumeForce;
+    public float maxVolumForce;
+    public float minPitch = 0;
+    public float maxPitch = 1.5f;
+
+
+    public void PlayCollisionSound(Collision collision) {
+        float force = collision.impulse.sqrMagnitude;
+        if (force < minVolumeForce) { return; }
+        force = Mathf.Clamp01((force - minVolumeForce) / (maxVolumForce - minVolumeForce));
+        float volume = minVolume + (maxVolume - minVolume) * force;
+        float randPitch = Random.Range(minPitch, maxPitch);
+        Debug.Log("CollisionSound recieved force of " + force + ", volume " + volume + ", pitch " + randPitch);
+
+        AudioSource child = Instantiate(collisionEffect, transform, true);
+        child.volume = volume;
+        child.pitch = randPitch;
+        child.Play();
+    }
+}
