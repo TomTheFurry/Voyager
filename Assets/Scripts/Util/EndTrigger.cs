49da700afbb6a97902e149ff3d2e8983698c3d2a
TomTheFurry <tomlee92502@yahoo.com>
2022-04-24 22:00:24 +0800

    Make end speed configable

--- Assets/Scripts/Util/EndTrigger.cs
+++ Assets/Scripts/Util/EndTrigger.cs
@@ -6,9 +6,10 @@ using UnityEngine.Events;
 public class EndTrigger : MonoBehaviour
 {
     public UnityEvent onEndTrigger;
+    public float minSpeed = float.MaxValue;
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player" && other.attachedRigidbody.velocity.magnitude < 1) {
+        if (other.tag == "Player" && other.attachedRigidbody.velocity.magnitude < minSpeed) {
             onEndTrigger.Invoke();
             enabled = false;
         }
