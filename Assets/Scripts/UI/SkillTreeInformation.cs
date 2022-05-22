68d028ff4e6acce2ea31fe261479129ac6f5232e
unknown <wongwaifung3701@gmail.com>
2022-05-22 15:13:28 +0800

    Skill tree information

--- Assets/Scripts/UI/SkillTreeInformation.cs
+++ Assets/Scripts/UI/SkillTreeInformation.cs
@@ -5,11 +5,37 @@ using UnityEngine.UI;
 
 public class SkillTreeInformation : MonoBehaviour
 {
-    public GameObject informationIcon;
+    public GameObject information;
+    public string objName;
     public GameObject icon;
+    public Sprite image;
+    public GameObject description;
 
     public void showInformation()
     {
-        informationIcon.GetComponent<Image>().sprite = icon.GetComponent<Image>().sprite;
+        Transform[] children = information.GetComponentsInChildren<Transform>();
+
+        foreach (Transform child in children)
+        {
+            string childName = child.gameObject.name;
+
+            if (string.Equals(childName, "Name"))
+            {
+                child.GetComponent<Text>().text = objName;
+            }
+            else if (string.Equals(childName, "Icon"))
+            {
+                child.GetComponent<Image>().sprite = icon.GetComponent<Image>().sprite;
+            }
+            else if (string.Equals(childName, "Image"))
+            {
+                child.GetComponent<Image>().sprite = image;
+            }
+            else if (string.Equals(childName, "Description"))
+            {
+                child.GetComponent<Text>().text = gameObject.GetComponent<Text>().text;
+            }
+        }
+        
     }
 }
