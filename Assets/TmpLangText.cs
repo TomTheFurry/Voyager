2b8f6a74ef3de84003afdc8cc67e6c3a96f7f8e3
TomTheFurry <tomlee92502@yahoo.com>
2022-07-17 14:26:58 +0800

    Make most UI texts use LangSystem.

--- Assets/TmpLangText.cs
+++ Assets/TmpLangText.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class TmpLangText : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GetComponent<TextMeshProUGUI>().text = LangSystem.parseText(GetComponent<TextMeshProUGUI>().text);
     }
 }
